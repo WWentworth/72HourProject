6aa90e0af7451fb153cf6fb3fd65d696a2cfca45
Maggie <88403063+maggiesnader@users.noreply.github.com>
2021-10-31 16:46:02 -0400

    reply model

--- 72HourProject/Models/Reply.cs
+++ 72HourProject/Models/Reply.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,15 @@ namespace _72HourProject.Models
 {
     public class Reply
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+        public virtual Comment Comment { get; set; }
+
+
+        public string Text { get; set; }
+        public Guid AuthorId { get; set; }
     }
 }
