21890c8cc1833c94f100b386389e17533cba6049
Maggie <88403063+maggiesnader@users.noreply.github.com>
2021-10-31 16:38:23 -0400

    post model

--- 72HourProject/Models/Post.cs
+++ 72HourProject/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace _72HourProject.Models
 {
     public class Post
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public Guid AuthorId { get; set; }
     }
 }
