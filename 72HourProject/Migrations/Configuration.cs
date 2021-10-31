b7b22f1ee3c4aebc6a3196534f9b08a6e6ef3494
Maggie <88403063+maggiesnader@users.noreply.github.com>
2021-10-31 16:51:46 -0400

    migration enabled, database updated

---
+++ 72HourProject/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace _72HourProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<_72HourProject.Models.UserDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(_72HourProject.Models.UserDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
